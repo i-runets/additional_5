056d347ff03e90d79e9d87c2db819fc617234291
seafiv <runecivan@gmail.com>
2018-02-20 21:07:08 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  str = str.split('');
+  var bracket = [];
+  if (str.length % 2 != 0) return false;
+  var i = 0;
+  while (i < str.length) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][1] && bracket[bracket.length - 1] == bracketsConfig[j][0]) bracket.pop();
+      else if (str[i] == bracketsConfig[j][0]) bracket.push(str[i])
+    }
+    i++
+  }
+  if (bracket.length == 0) return true;
+  else return false;
 }
